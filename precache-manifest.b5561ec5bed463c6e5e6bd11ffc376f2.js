4e3c32a09d911e91e6b447bf33836e0b28c51d32
AD <atar.dci@gmail.com>
2019-04-04 16:14:01 +0200

    Updates

---
+++ precache-manifest.b5561ec5bed463c6e5e6bd11ffc376f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/memo-list/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b965bae3fe9d85244422",
+    "url": "/memo-list/static/js/runtime~main.ff667840.js"
+  },
+  {
+    "revision": "9a596066363b7b2fdf59",
+    "url": "/memo-list/static/js/main.64dd50a3.chunk.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/memo-list/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "9a596066363b7b2fdf59",
+    "url": "/memo-list/static/css/main.2a224fbe.chunk.css"
+  },
+  {
+    "revision": "6b91fbde48364ddfd22d47ba016b3fd7",
+    "url": "/memo-list/index.html"
+  }
+];
